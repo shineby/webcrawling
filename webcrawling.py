d6ed6721b262642d5ac96c352526e440e2151d36
shineby <shineby@gmail.com>
2018-04-26 15:36:33 +0900

    add dataframe

--- webcrawling.py
+++ webcrawling.py
@@ -1,9 +1,10 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from openpyxl import load_workbook
 
 # http://land.naver.com/article/molitPriceInfo.nhn?rletTypeCd=A01&tradeTypeCd=A1&rletNo=3211&cortarNo=1153010100&hscpTypeCd=A01%3AA03%3AA04&mapX=126.8843578&mapY=37.5123053&mapLevel=13&page=&articlePage=&ptpNo=&rltrId=&mnex=&bildNo=&articleOrderCode=&cpId=&period=&prodTab=&atclNo=&atclRletTypeCd=&location=700&bbs_tp_cd=&sort=&siteOrderCode=&schlCd=&tradYy=2017&exclsSpc=&splySpcR=&cmplYn=
-year = '2017'
+year = '2015'
 response = requests.get(
     'http://land.naver.com/article/molitPriceInfo.nhn?rletTypeCd=A01&tradeTypeCd=A1&rletNo=3211&cortarNo=1153010100&hscpTypeCd=A01%3AA03%3AA04&mapX=126.8843577&mapY=37.5123143&mapLevel=13&page=&articlePage=&ptpNo=&rltrId=&mnex=&bildNo=&articleOrderCode=&cpId=&period=&prodTab=&atclNo=&atclRletTypeCd=&location=300&bbs_tp_cd=&sort=&siteOrderCode=&schlCd=&tradYy=' + year + '&exclsSpc=&splySpcR=&cmplYn=')
 html = response.text
@@ -36,7 +37,11 @@ for i in item_data:
         if j.text == '':
             print("-")
         else:
-            temp.append(j.text)
+            tmp = j.text.replace(",", "")
+            if tmp.find('/') != -1:
+                # print("!!!" + tmp)
+                continue
+            temp.append(int(tmp))
 
     result_price.append(temp)
 
@@ -45,5 +50,17 @@ for i in result_month:
     print(result_month[index], result_price[index])
     index = index + 1
 
-tmp = pd.DataFrame(result_price, index=result_month)
+total = {"date": result_month, "price": result_price}
+tmp = pd.DataFrame(total)
 print(tmp)
+
+# book = load_workbook('신도림현대.xlsx')
+# with pd.ExcelWriter('신도림현대.xlsx', engine='openpyxl') as writer:
+#     writer.book = book
+#     writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+#
+#     ## Your dataframe to append.
+#     tmp.to_excel(writer, year)
+#     writer.save()
+# #tmp.to_excel('신도림현대.xlsx', sheet_name=year)
+# print(tmp)
