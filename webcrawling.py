3ff48dc58ef53a352d8c6d33c40ede7ff767885b
shineby <shineby@gmail.com>
2018-04-26 12:13:44 +0900

    add pandas DataFrame and year
    add pandas DataFrame and year

--- webcrawling.py
+++ webcrawling.py
@@ -1,9 +1,11 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
-from queue import Empty
 
+# http://land.naver.com/article/molitPriceInfo.nhn?rletTypeCd=A01&tradeTypeCd=A1&rletNo=3211&cortarNo=1153010100&hscpTypeCd=A01%3AA03%3AA04&mapX=126.8843578&mapY=37.5123053&mapLevel=13&page=&articlePage=&ptpNo=&rltrId=&mnex=&bildNo=&articleOrderCode=&cpId=&period=&prodTab=&atclNo=&atclRletTypeCd=&location=700&bbs_tp_cd=&sort=&siteOrderCode=&schlCd=&tradYy=2017&exclsSpc=&splySpcR=&cmplYn=
+year = '2017'
 response = requests.get(
-    'http://land.naver.com/article/molitPriceInfo.nhn?rletTypeCd=A01&tradeTypeCd=A1&rletNo=3211&cortarNo=1153010100&hscpTypeCd=A01%3AA03%3AA04&mapX=126.8843577&mapY=37.5123143&mapLevel=13&page=&articlePage=&ptpNo=&rltrId=&mnex=&bildNo=&articleOrderCode=&cpId=&period=&prodTab=&atclNo=&atclRletTypeCd=&location=300&bbs_tp_cd=&sort=&siteOrderCode=&schlCd=&tradYy=&exclsSpc=&splySpcR=&cmplYn=')
+    'http://land.naver.com/article/molitPriceInfo.nhn?rletTypeCd=A01&tradeTypeCd=A1&rletNo=3211&cortarNo=1153010100&hscpTypeCd=A01%3AA03%3AA04&mapX=126.8843577&mapY=37.5123143&mapLevel=13&page=&articlePage=&ptpNo=&rltrId=&mnex=&bildNo=&articleOrderCode=&cpId=&period=&prodTab=&atclNo=&atclRletTypeCd=&location=300&bbs_tp_cd=&sort=&siteOrderCode=&schlCd=&tradYy=' + year + '&exclsSpc=&splySpcR=&cmplYn=')
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
@@ -38,5 +40,10 @@ for i in item_data:
 
     result_price.append(temp)
 
-print(result_month)
-print(result_price)
+index = 0
+for i in result_month:
+    print(result_month[index], result_price[index])
+    index = index + 1
+
+tmp = pd.DataFrame(result_price, index=result_month)
+print(tmp)
